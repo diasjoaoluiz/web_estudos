5a0f1f870a17ebcd42c3dd1c9c8978cf941f7972
alexadre-silvaa <alexandrebsilva9@gmail.com>
2023-02-13 16:58:52 -0300

    teste de arquivo php

--- phps/email.php
+++ phps/email.php
@@ -1,16 +1,24 @@
 <?php
-
     if(isset($_POST['email']) && !empty($_POST['email'])){
 
-    $nome = addcslashes($_POST['name'])
-    $email = addcslashes($POST['email'])
-    $mensagem = addcslashes($_POST['message'])
+    $nome = addcslashes($_POST['name']);
+    $email = addcslashes($POST['email']);
+    $mensagem = addcslashes($_POST['message']);
 
-    $t0 = "joao_dacosta@live.com";
+    
+    $to = "joao_dacosta@live.com";
     $subject = "Contato meu Site";
     $body = "Nome :".$nome. "\r\n"
             "Email: ".$email. "\r\n"
             "Mensagem: ".$mensagem;
+    $header = "From:"joao_dacosta@live.com"."\r\r"
+                     ."Reply-To:".$email."\r\n"
+                     ."X=Mailer:PHP/".phpversion();
+
+    if(mail($to,$subject,$body,$header(){
+        echo("Deu certo");
+    }
+    
 
     }
 
