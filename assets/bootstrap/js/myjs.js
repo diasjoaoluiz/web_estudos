83195ebf0d2768faed08e9c7950a32e68344d18f
diasjoaoluiz <joao_dacosta@live.com>
2023-02-15 17:18:16 -0300

    Alterações about e novas informações sobre skils no index

---
+++ assets/bootstrap/js/myjs.js
@@ -0,0 +1,5 @@
+function alerta_email(){
+    <div class="alert alert-danger" role="alert">
+        A simple danger alert—check it out!
+    </div>
+}
--- phps/email.php
+++ phps/email.php
@@ -2,25 +2,22 @@
     if(isset($_POST['email']) && !empty($_POST['email'])){
 
     $nome = addcslashes($_POST['name']);
-    $email = addcslashes($POST['email']);
+    $email = addcslashes($_POST['email']);
     $mensagem = addcslashes($_POST['message']);
 
-    
     $to = "joao_dacosta@live.com";
     $subject = "Contato meu Site";
-    $body = "Nome :".$nome. "\r\n"
-            "Email: ".$email. "\r\n"
-            "Mensagem: ".$mensagem;
-    $header = "From:"joao_dacosta@live.com"."\r\r"
-                     ."Reply-To:".$email."\r\n"
-                     ."X=Mailer:PHP/".phpversion();
+    $body = "Nome: ".$nome. "\r\n"."Email: ".$email."\r\n"."Mensagem: ".$mensagem;
+    $header = "From:joao_dacosta@live.com"."\r\r"."Reply-To:".$email."\r\n"."X=Mailer:PHP/".phpversion();
 
-    if(mail($to,$subject,$body,$header(){
-        echo("Deu certo");
-    }
     
-
+    if(mail($to,$subject,$body,$header)){
+        echo "<meta http-equiv='refresh' content='1;URL=../tabs/about.html'>";
+        echo "<script>alert('E-mail enviado!')</script>";
+    }else{
+        echo "<meta http-equiv='refresh' content='1;URL=../tabs/about.html'>";
+        echo "<script>alert('E-mail não enviado!')</script>";
     }
-
+    }
 
     ?>
